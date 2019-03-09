d34e4aab545e957f38e4d701cd22e890752e4bef
Leo.Huang <464362353@qq.com>
2019-03-09 20:01:57 +0800

    [A] 注册模块：状态渲染.

--- src/pages/signup/index.tsx
+++ src/pages/signup/index.tsx
@@ -1,30 +1,64 @@
 import * as React from "react";
+import { withRouter } from 'react-router';
 import { fetchSendEmail, fetchSignUp, fetchVerifyCode, fetchVerifyEmail } from "../../request";
+import { onTipHandle } from "src/utils/Methods";
 import "./index.scss";
+import Tip from "src/component/Tip";
 
-class SignUp extends React.Component<{}, any> {
+class SignUp extends React.Component<PropsType, StateType> {
+  constructor(props) {
+    super(props);
+    this.state = {
+      nickname: null,
+      email: null,
+      password: null,
+      vCode: null,
+      tipText: null,
+    };
+  }
 
-  sendCode = () => {
-    async function verifyEmail() {
-      let res = await fetchVerifyEmail(this.state.email);
-      if (res.data.status === 0) {
-        fetchSendEmail(this.state.email);
-      }
-      //  这里如果是已注册，需要提示。
+  /** 发送验证码 **/
+  sendCode = async () => {
+    let {email} = this.state;
+    if (!email) {
+      onTipHandle.call(this, "邮箱不能为空");
+      return;
     }
 
-    verifyEmail.bind(this)();
+    let res = await fetchVerifyEmail(email);
+    if (res.status === 0) {
+      fetchSendEmail(email)
+        .then(e => {
+          onTipHandle.call(this, e.message);
+        });
+      return;
+    }
+    onTipHandle.call(this, res.message);
   };
 
-  signup = () => {
-    (async () => {
-      let res = await fetchVerifyCode(this.state.email, this.state.vCode);
-      if (!res.data.status)
-        return res.data.status;
-      fetchSignUp(this.state);
-    })()
+  /** 注册提交 **/
+  signup = async () => {
+    for (let key in this.state) {
+      if (!this.state[key] && key !== "tipText") {
+        onTipHandle.call(this, "信息不全，无法提交");
+        return;
+      }
+    }
+    const {email, vCode} = this.state;
+    let res = await fetchVerifyCode(email, vCode);
+    if (!res.status) {
+      onTipHandle.call(this, res.message);
+      return;
+    }
+    let result = await fetchSignUp(this.state);
+    onTipHandle.call(this, result.message, () => {
+      if (result.status) {
+        this.props.history.push('/signin')
+      }
+    });
   };
 
+  /** 表单输入 **/
   onInput = (key, val) => {
     this.setState({
       [key]: val
@@ -32,13 +66,13 @@ class SignUp extends React.Component<{}, any> {
   };
 
   render() {
+    const {tipText} = this.state;
+
     return (
       <div className="signup-wrap content">
         <div className='main'>
-
           <div className="form-wrap">
             <form action="" acceptCharset="UTF-8" method="POST" target="iframe_none">
-
               <div className="form-fields">
                 <fieldset>
                   <label htmlFor="account">Name:</label>
@@ -94,17 +128,32 @@ class SignUp extends React.Component<{}, any> {
                 tabIndex={6}
                 onClick={this.signup}
               />
+
               <iframe id="iframe_none" name="iframe_none"/>
             </form>
+
             <p>
               Creating an account means you’re okay with our Terms of Service, Privacy Policy, and our default Notification Settings.
             </p>
           </div>
-
         </div>
+
+        <Tip text={tipText}/>
       </div>
     );
   }
 }
 
-export default SignUp;
+interface PropsType {
+  history: any
+}
+
+interface StateType {
+  nickname?: string;
+  email?: string;
+  password?: string;
+  vCode?: string | number;
+  tipText?: string;
+}
+
+export default withRouter(SignUp);
